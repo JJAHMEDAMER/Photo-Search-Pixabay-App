de66f9b5d968463c72757e0596db2b61e5c95bd5
JJAHMEDAMER <ahmedamer703.aa@gmail.com>
2022-10-15 00:14:15 +0200

    Custom TextField Widget

---
+++ lib/widgets/AppInputField.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:photo_search_pixabay_app/utils/colors.dart';
+
+class AppInputField extends StatelessWidget {
+  final Color color;
+  final double size;
+  final FontWeight fontWeight;
+  final String fontFamily;
+
+  const AppInputField({
+    super.key,
+    this.color = AppColors.accent,
+    this.size = 16,
+    this.fontWeight = FontWeight.w600,
+    this.fontFamily = 'Raleway',
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      cursorWidth: 2.2,
+      cursorColor: AppColors.darkGreen,
+      style: TextStyle(
+        color: color,
+        fontSize: size,
+        fontWeight: fontWeight,
+        fontFamily: fontFamily,
+      ),
+      decoration: InputDecoration(
+        // Fill Color
+        filled: true,
+        fillColor: AppColors.darkBlue,
+
+        //Border
+        border: OutlineInputBorder(
+          borderSide: BorderSide.none,
+          borderRadius: BorderRadius.circular(15),
+        ),
+
+        // Hint
+        hintText: "Type Here ...",
+        hintStyle: TextStyle(
+          color: color.withOpacity(0.5),
+          fontSize: size,
+          fontWeight: fontWeight,
+          fontFamily: fontFamily,
+        ),
+        helperText: "Search For Any Image",
+        helperStyle: TextStyle(
+          color: color.withOpacity(0.8),
+          fontSize: 11,
+          fontWeight: fontWeight,
+          fontFamily: fontFamily,
+        ),
+      ),
+    );
+  }
+}

52cbe2d10ef61996e4486401af546028d15f99c4
JJAHMEDAMER <ahmedamer703.aa@gmail.com>
2022-10-15 00:14:40 +0200

    Clean Layout

--- lib/pages/search_view.dart
+++ lib/pages/search_view.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:photo_search_pixabay_app/utils/colors.dart';
+import 'package:photo_search_pixabay_app/widgets/AppButton.dart';
+import 'package:photo_search_pixabay_app/widgets/AppInputField.dart';
+import 'package:photo_search_pixabay_app/widgets/AppText.dart';
 
 class SearchView extends StatelessWidget {
   const SearchView({super.key});
@@ -6,15 +10,30 @@ class SearchView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Text("PixaBay API Search"),
-          TextField(),
-          ElevatedButton(
-            onPressed: () {},
-            child: Text("Search"),
-          ),
-        ],
+      appBar: AppBar(
+        title: Text("PixaPay!"),
+        centerTitle: true,
+        toolbarHeight: 50,
+        backgroundColor: AppColors.darkBlue,
+      ),
+      backgroundColor: AppColors.blue,
+      body: Container(
+        margin: const EdgeInsets.all(15),
+        height: double.maxFinite,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            const AppText(
+              text: "PixaBay API Search",
+              size: 26,
+            ),
+            AppInputField(),
+            AppButton(
+              text: "Search",
+              size: 18,
+            ),
+          ],
+        ),
       ),
     );
   }
